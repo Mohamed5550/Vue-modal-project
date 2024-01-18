f713f7d94bb95b2f451aed962cfed2b184fd2627
Mohamed Hesham <mohamed.hesham5550@gmail.com>
2024-01-18 14:18:05 +0200

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,44 @@
+<template>
+  <h1>{{ title }}</h1>
+  <div v-if="showModal">
+    <Modal header="This is a test header text" @close="toggleModal">
+      <h1>Modal title</h1>
+      <p>Modal content</p>
+    </Modal>
+  </div>
+
+  <button @click="toggleModal">Show modal</button>
+</template>
+
+<script>
+
+import Modal from './components/Modal.vue';
+import './assets/global.css';
+
+export default {
+  name: 'App',
+  components: {Modal},
+  data() {
+    return {
+      title: "My first vue app",
+      showModal: false
+    }
+  },
+  methods: {
+    toggleModal() {
+      this.showModal = !this.showModal
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Modal.vue
@@ -0,0 +1,43 @@
+<template>
+    <div class="backdrop" @click.self="emitClose">
+        <div class="modal">
+            <slot></slot>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+h1 {
+    color: red;
+}
+
+.backdrop {
+    background: #24242480;
+    width: 100%;
+    height: 100%;
+    position: fixed;
+    top: 0;
+    left: 0
+}
+
+.modal {
+    background: #FFF;
+    padding: 20px;
+    margin: 20px;
+    border-radius: 10px;
+    display: inline-block;
+}
+</style>
+
+<script>
+
+export default {
+    props: ['header'],
+    methods: {
+        emitClose() {
+            this.$emit("close");
+        }
+    }
+}
+
+</script>
